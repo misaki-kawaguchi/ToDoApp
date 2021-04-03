68614a0a7f9a5c40bbacb0409ac0906419a2680b
川口美咲 <misaki@arcphilia.co.jp>
2021-04-04 00:21:49 +0900

    [Change]Packageを作成して移動

--- app/src/main/java/com/misakikawaguchi/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/misakikawaguchi/todoapp/fragments/list/ListFragment.kt
@@ -1,18 +1,20 @@
-package com.misakikawaguchi.todoapp
+package com.misakikawaguchi.todoapp.fragments.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.misakikawaguchi.todoapp.R
 
-class AddFragment : Fragment() {
+class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
+
 }
--- app/src/main/java/com/misakikawaguchi/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/misakikawaguchi/todoapp/fragments/update/UpdateFragment.kt
@@ -1,10 +1,11 @@
-package com.misakikawaguchi.todoapp
+package com.misakikawaguchi.todoapp.fragments.update
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.misakikawaguchi.todoapp.R
 
 class UpdateFragment : Fragment() {
 
