27bfd2a3604e009f96af6a1f81d360e391bb49ed
川口美咲 <misaki@arcphilia.co.jp>
2021-04-04 22:41:24 +0900

    [Add]ToDoDao作成

---
+++ app/src/main/java/com/misakikawaguchi/todoapp/data/ToDoDao.kt
@@ -0,0 +1,23 @@
+package com.misakikawaguchi.todoapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+// Databaseにアクセスするためのメソッドを格納するオブジェクト、インターフェース
+@Dao
+interface ToDoDao {
+
+    // LiveData：監視可能なデータホルダークラス、データに変更があった場合はObserver(Fragment)に通知しUIをアップデートしてくれる
+    // データベーステーブルからデータを受け取る（todo_tableからidを昇順で取得）
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<ToDoData>>
+
+    // データを挿入
+    // onConflict = OnConflictStrategy.IGNORE：コンフリクトが発生しても無視し、処理を継続する
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    // suspend:現在のコルーチンの実行を一時停止し、すべてのローカル変数を保存
+    suspend fun insertData(toDoData: ToDoData)
+}
