2a5658c62bf2dfb0f452aec162a5f07c4df7f216
川口美咲 <misaki@arcphilia.co.jp>
2021-04-04 20:51:05 +0900

    [Add]AddFragmentにメニューを追加

--- app/src/main/java/com/misakikawaguchi/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/misakikawaguchi/todoapp/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.misakikawaguchi.todoapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.misakikawaguchi.todoapp.R
 
 class AddFragment : Fragment() {
@@ -13,7 +11,16 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // メニューを設定する
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    // オプションメニューを表示する
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 }
