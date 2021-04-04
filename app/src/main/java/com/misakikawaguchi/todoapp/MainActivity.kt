a12408b18d5b28a89dbfc70909224423d4d5fd9c
川口美咲 <misaki@arcphilia.co.jp>
2021-04-04 16:15:16 +0900

    [Add]Upボタン追加、labelをタイトルとして表示する

--- app/src/main/java/com/misakikawaguchi/todoapp/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/todoapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.misakikawaguchi.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 自動でUpボタン（←）を追加、xmlで指定したlabelをタイトルとして表示
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    // Upボタン（←）が機能するようになる
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
