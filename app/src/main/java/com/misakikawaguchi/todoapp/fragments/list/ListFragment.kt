b1de09b8b2cc75238a4eec48d1505e76e22981cf
川口美咲 <misaki@arcphilia.co.jp>
2021-04-08 21:46:44 +0900

    [Add]データに変更があった場合は更新されるようにする

--- app/src/main/java/com/misakikawaguchi/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/misakikawaguchi/todoapp/fragments/list/ListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.misakikawaguchi.todoapp.R
@@ -30,6 +31,12 @@ class ListFragment : Fragment() {
         recyclerView.adapter = adapter
         // LayoutManagerの設定
         recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        // fragmentで監視するときは引数にviewLifecycleOwnerを渡す
+        // データに変更があった場合は更新される（ListAdapter.kt）
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            adapter.setData(data)
+        })
 
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
