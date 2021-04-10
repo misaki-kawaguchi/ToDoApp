9950e2ab860da9ed9cf9b3934294f50dbdbd6d21
川口美咲 <misaki@arcphilia.co.jp>
2021-04-10 23:39:58 +0900

    [Add]Data Binding(BindingAdapters.kt)

--- app/src/main/java/com/misakikawaguchi/todoapp/fragments/BindingAdapters.kt
+++ app/src/main/java/com/misakikawaguchi/todoapp/fragments/BindingAdapters.kt
@@ -2,12 +2,16 @@ package com.misakikawaguchi.todoapp.fragments
 
 import android.view.View
 import android.widget.Spinner
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.misakikawaguchi.todoapp.R
 import com.misakikawaguchi.todoapp.data.models.Priority
+import com.misakikawaguchi.todoapp.data.models.ToDoData
+import com.misakikawaguchi.todoapp.fragments.list.ListFragmentDirections
 
 class BindingAdapters {
 
@@ -39,6 +43,25 @@ class BindingAdapters {
                 Priority.HIGH -> { view.setSelection(0) }
                 Priority.MEDIUM -> { view.setSelection(1) }
                 Priority.LOW -> { view.setSelection(2) }
+            }
+        }
+
+        @BindingAdapter("android:parsePriorityColor")
+        @JvmStatic
+        fun parsePriorityColor(cardView: CardView, priority: Priority){
+            when(priority){
+                Priority.HIGH -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.red)) }
+                Priority.MEDIUM -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.yellow)) }
+                Priority.LOW -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.green)) }
+            }
+        }
+
+        @BindingAdapter("android:sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view: ConstraintLayout, currentItem: ToDoData){
+            view.setOnClickListener {
+                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
+                view.findNavController().navigate(action)
             }
         }
     }
