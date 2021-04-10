be891cdec0a9b14e32a04a56f7f712f0eb45541a
川口美咲 <misaki@arcphilia.co.jp>
2021-04-10 23:07:49 +0900

    [Add]DataBinding(title, parsePriorityToInt, description)

--- app/src/main/java/com/misakikawaguchi/todoapp/fragments/BindingAdapters.kt
+++ app/src/main/java/com/misakikawaguchi/todoapp/fragments/BindingAdapters.kt
@@ -1,11 +1,13 @@
 package com.misakikawaguchi.todoapp.fragments
 
 import android.view.View
+import android.widget.Spinner
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.misakikawaguchi.todoapp.R
+import com.misakikawaguchi.todoapp.data.models.Priority
 
 class BindingAdapters {
 
@@ -30,6 +32,14 @@ class BindingAdapters {
             }
         }
 
+        @BindingAdapter("android:parsePriorityToInt")
+        @JvmStatic
+        fun parsePriorityToInt(view: Spinner, priority: Priority){
+            when(priority){
+                Priority.HIGH -> { view.setSelection(0) }
+                Priority.MEDIUM -> { view.setSelection(1) }
+                Priority.LOW -> { view.setSelection(2) }
+            }
+        }
     }
-
 }
