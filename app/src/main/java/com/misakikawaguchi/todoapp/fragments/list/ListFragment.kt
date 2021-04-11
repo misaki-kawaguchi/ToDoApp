7daf0fd9d9d17b5f4d8eadb17c454491376f513a
川口美咲 <misaki@arcphilia.co.jp>
2021-04-11 18:19:58 +0900

    [Add]スワイプしてリストを削除できるようにする

--- app/src/main/java/com/misakikawaguchi/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/misakikawaguchi/todoapp/fragments/list/ListFragment.kt
@@ -7,13 +7,14 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.misakikawaguchi.todoapp.R
 import com.misakikawaguchi.todoapp.data.viewmodel.ToDoViewModel
 import com.misakikawaguchi.todoapp.databinding.FragmentListBinding
 import com.misakikawaguchi.todoapp.fragments.SharedViewModel
-import kotlinx.android.synthetic.main.fragment_list.view.*
+import com.misakikawaguchi.todoapp.fragments.list.adapter.ListAdapter
 
 class ListFragment : Fragment() {
 
@@ -58,6 +59,22 @@ class ListFragment : Fragment() {
         recyclerView.adapter = adapter
         // LayoutManagerの設定
         recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        swipeToDelete(recyclerView)
+    }
+
+    // スワイプしてデータを削除する
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+        val swipeToDeleteCallback = object : SwipeToDelete() {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val itemToDelete = adapter.dataList[viewHolder.adapterPosition]
+                // データを削除する
+                mToDoViewModel.deleteItem(itemToDelete)
+                Toast.makeText(requireContext(), "Successfully Removed: '${itemToDelete.title}'", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
     // オプションメニューを表示する
